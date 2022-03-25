b4c7932e0af1411a0f4afabea71e800d53f4660e
Adrien VRD <adrien.vaucard@gmail.com>
2022-03-25 11:48:14 +0100

    test(sum): add ssum test

--- src/entities/calculator.ts
+++ src/entities/calculator.ts
@@ -1,9 +1,26 @@
 export class Calculator {
     constructor() {}
 
-    sum(a: number, b: number) {
-        return a + b;
+    sum(a: number, b: number): number {
+        let result = a + b;
+        return Math.round(result * 100) / 100;
     }
+
+    // isAmountEnough(drink: Drink, amount: number): boolean {
+    //     if (amount < drink.price) {
+    //         return false;
+    //     } else {
+    //         return true;
+    //     }
+    // }
+
+    // amountRemaining(drink: Drink, amount: number): number {
+    //     if (amount < drink.price) {
+    //         return drink.price - amount;
+    //     } else {
+    //         return 0;
+    //     }
+    // }
 }
 
 export default Calculator;
---
+++ tests/calculator.test.ts
@@ -0,0 +1,17 @@
+import { Calculator } from "../src/entities/calculator";
+
+describe('Calculator tests', () => {
+    let calculator: Calculator;
+
+    beforeEach(() => calculator = new Calculator());
+
+    it('adds 10 cts + 20 cts = 30 cts', () => {
+        
+      expect(calculator.sum(0.1, 0.2)).toBe(0.3);
+    })
+
+    it('adds 10 cts + 20 cts = 30 cts', () => {
+        
+      expect(calculator.sum(0.1, 0.2)).toBe(0.3);
+    })
+});
